57261ada05b7a48836511761fef9de96fcd49c33
miroa <miroa@LAPTOP-B4MICR32>
2023-01-19 09:35:09 +0200

    Add project files.

---
+++ päivienero/Program.cs
@@ -0,0 +1,28 @@
+﻿DateTime nyt = DateTime.Now;
+DateTime a = new DateTime();
+int years;
+int months;
+
+
+Console.WriteLine("syötä päivämäärää");
+string paivam=Console.ReadLine();
+if (!DateTime.TryParse(paivam, out a))
+{
+    Console.WriteLine("Yritä uudelleen");
+}
+TimeSpan intervalli = nyt - a;
+int days = Math.Abs(intervalli.Days-1);
+if (days>=365)
+{
+    years= days/365;
+    Console.WriteLine("Vuodet {0}", years);
+
+}
+if (days>=30)
+{
+    months= days/30;
+    Console.WriteLine("kuukaudet {0}", months);
+}
+Console.WriteLine("Päivät {0}",days);
+
+
