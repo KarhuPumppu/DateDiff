fa73c204350d3e1844804797ed27060278c3fb3c
miroa <miroa@LAPTOP-B4MICR32>
2023-01-24 08:40:55 +0200

    en saanut vuosien laskemista toimimaan.

--- päivienero/Program.cs
+++ päivienero/Program.cs
@@ -12,17 +12,6 @@ if (!DateTime.TryParse(paivam, out a))
 }
 TimeSpan intervalli = nyt - a;
 int days = Math.Abs(intervalli.Days-1);
-if (days>=365)
-{
-    years= days/365;
-    Console.WriteLine("Vuodet {0}", years);
-
-}
-if (days>=30)
-{
-    months= days/30;
-    Console.WriteLine("kuukaudet {0}", months);
-}
 Console.WriteLine("Päivät {0}",days);
 
 
